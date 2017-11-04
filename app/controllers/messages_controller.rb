6e442cb5a5a643738275d71f51f5e808550feb56
alexponi <alexponi@mail.ru>
2017-11-05 00:34:21 +0500

    General view of the page

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+
+  def index
+    @message = Message.new
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to @message, notice: 'Message was successfully created.' }
+        format.json { render :show, status: :created, location: @message }
+      else
+        format.html { render :new }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:text)
+    end
+end
